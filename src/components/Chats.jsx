b33adb34e5d0fba636db0efb4712de33d15fa1ea
hashir1111 <110687639+hashir1111@users.noreply.github.com>
2023-12-28 02:51:55 +0500

    changes in Chat.jsx

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -4,13 +4,26 @@ import { ChatEngine } from "react-chat-engine";
 import { auth } from "../firebase";
 
 const Chats = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
   return (
     <div className="chats-page">
       <div className="nav-bar">
         <div className="logo-tab">ChatApp</div>
-        <div className="logout-tab">Logout</div>
+        <div onClick={handleLogout} className="logout-tab">
+          Logout
+        </div>
       </div>
-      <ChatEngine height="calc(100vh - 66px)" />
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectId="791a8823-3361-4b14-b9d7-b137fb475978"
+        userName="."
+        userSecret="."
+      />
     </div>
   );
 };
